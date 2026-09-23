9b93bbdf4ad441c72b00a7110247dbd7b577ff93
nitishratre45 <nitishratre45@gmail.com>
2026-09-24 00:29:12 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,4 +1,6 @@
-{
-  "status": "ok",
-  "service": "sony.webp-api"
+export default function handler(req, res) {
+  return res.status(200).json({
+    status: "ok",
+    service: "Sonycriczone"
+  });
 }
